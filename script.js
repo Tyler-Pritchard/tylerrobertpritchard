3c74f7c21d93d3f2bc386d18410e2f0c60af099a
Tyler Pritchard <pritchard.tyler@gmail.com>
2020-03-23 13:07:53 -0700

    fixed bg-size bug

---
+++ script.js
@@ -0,0 +1,6 @@
+// let light = document.getElementById('bg-video__light');
+// let dark = document.getElementById('bg-video__dark');
+
+// document.querySelector('.btn-theme').addEventListener('toggle', () => {
+    
+// })
